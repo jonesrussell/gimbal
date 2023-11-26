8365fb65d701b9e8bbd023cb23617f4ce2c4391a
Russell Jones <jonesrussell42@gmail.com>
2023-11-25 22:09:33 -0500

    Add ebitengine-input

--- main.go
+++ main.go
@@ -1,30 +1,86 @@
 package main
 
 import (
+	"image"
 	"log"
 
 	"github.com/hajimehoshi/ebiten/v2"
 	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
+
+	input "github.com/quasilyte/ebitengine-input"
 )
 
-type Game struct{}
-
-func (g *Game) Update() error {
-	return nil
-}
-
-func (g *Game) Draw(screen *ebiten.Image) {
-	ebitenutil.DebugPrint(screen, "Hello, World!")
-}
-
-func (g *Game) Layout(outsideWidth, outsideHeight int) (screenWidth, screenHeight int) {
-	return 320, 240
-}
+const (
+	ActionMoveLeft input.Action = iota
+	ActionMoveRight
+	ActionMoveUp
+	ActionMoveDown
+)
 
 func main() {
 	ebiten.SetWindowSize(640, 480)
-	ebiten.SetWindowTitle("Hello, World!")
-	if err := ebiten.RunGame(&Game{}); err != nil {
+	if err := ebiten.RunGame(newExampleGame()); err != nil {
 		log.Fatal(err)
 	}
+}
+
+type exampleGame struct {
+	p           *player
+	inputSystem input.System
+}
+
+func newExampleGame() *exampleGame {
+	g := &exampleGame{}
+	g.inputSystem.Init(input.SystemConfig{
+		DevicesEnabled: input.AnyDevice,
+	})
+	keymap := input.Keymap{
+		ActionMoveLeft:  {input.KeyGamepadLeft, input.KeyLeft, input.KeyA},
+		ActionMoveRight: {input.KeyGamepadRight, input.KeyRight, input.KeyD},
+		ActionMoveUp:    {input.KeyGamepadUp, input.KeyUp, input.KeyW},
+		ActionMoveDown:  {input.KeyGamepadDown, input.KeyDown, input.KeyS},
+	}
+	g.p = &player{
+		input: g.inputSystem.NewHandler(0, keymap),
+		pos:   image.Point{X: 96, Y: 96},
+	}
+	return g
+}
+
+func (g *exampleGame) Layout(outsideWidth, outsideHeight int) (int, int) {
+	return 640, 480
+}
+
+func (g *exampleGame) Draw(screen *ebiten.Image) {
+	g.p.Draw(screen)
+}
+
+func (g *exampleGame) Update() error {
+	g.inputSystem.Update()
+	g.p.Update()
+	return nil
+}
+
+type player struct {
+	input *input.Handler
+	pos   image.Point
+}
+
+func (p *player) Update() {
+	if p.input.ActionIsPressed(ActionMoveLeft) {
+		p.pos.X -= 4
+	}
+	if p.input.ActionIsPressed(ActionMoveRight) {
+		p.pos.X += 4
+	}
+	if p.input.ActionIsPressed(ActionMoveUp) {
+		p.pos.Y -= 4
+	}
+	if p.input.ActionIsPressed(ActionMoveDown) {
+		p.pos.Y += 4
+	}
+}
+
+func (p *player) Draw(screen *ebiten.Image) {
+	ebitenutil.DebugPrintAt(screen, "PlayeR", p.pos.X, p.pos.Y)
 }
