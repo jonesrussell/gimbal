c25b674300f764963c78664b66d7eed8e8eb4a8c
Russell Jones <499552+jonesrussell@users.noreply.github.com>
2024-03-14 01:09:39 +0000

    Move the rest of debug functions to debug.go

--- internal/game/debug.go
+++ internal/game/debug.go
@@ -5,7 +5,12 @@ import (
 	"image/color"
 
 	"github.com/hajimehoshi/ebiten/v2"
+	"github.com/hajimehoshi/ebiten/v2/ebitenutil"
 	"github.com/hajimehoshi/ebiten/v2/vector"
+)
+
+const (
+	debugGridSpacing = 32
 )
 
 // DebugPrintStar prints the debug information for a star.
@@ -24,5 +29,23 @@ func DrawDebugGridOverlay(screen *ebiten.Image) {
 		for i := 0; i < screenHeight; i += debugGridSpacing {
 			vector.StrokeLine(screen, 0, float32(i), float32(screenWidth), float32(i), 1, color.White, false)
 		}
+	}
+}
+
+func (g *GimlarGame) DrawDebugInfo(screen *ebiten.Image) {
+	// Print the current FPS
+	ebitenutil.DebugPrint(screen, fmt.Sprintf("FPS: %0.2f", ebiten.ActualFPS()))
+
+	// Draw grid overlay
+	g.DrawDebugGrid(screen)
+}
+
+func (g *GimlarGame) DrawDebugGrid(screen *ebiten.Image) {
+	// Draw grid overlay
+	for i := 0; i < screenWidth; i += debugGridSpacing {
+		vector.StrokeLine(screen, float32(i), 0, float32(i), float32(screenHeight), 1, color.White, false)
+	}
+	for i := 0; i < screenHeight; i += debugGridSpacing {
+		vector.StrokeLine(screen, 0, float32(i), float32(screenWidth), float32(i), 1, color.White, false)
 	}
 }
